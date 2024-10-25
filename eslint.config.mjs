571e131c3555f8e7c51ca193e79f1aa07c7b9642
cewald <c.ewald@spiri.bo>
2024-10-25 07:34:22 +0200

    feat: add custom dev tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,16 @@
-// @ts-check
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import config from '@cewald/eslint-config'
+
+const corporateConfig = config()
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
   features: {
-    // Rules for module authors
     tooling: true,
-    // Rules for formatting
-    stylistic: true,
   },
   dirs: {
     src: [
       './playground',
     ],
   },
-})
-  .append(
-    // your custom flat config here...
-  )
+}).append(corporateConfig)
